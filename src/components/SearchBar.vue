431e4ae6dedf3ed8f61b571d715762e4485ca198
Tikshan Mohan <tik.mohan11@gmail.com>
2022-07-12 18:08:24 +0200

    Added Styling to the SearchBar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <input type="text"/>
+  <input class= "search" type="text" placeholder="Search..."/>
 </template>
 
 <script>
@@ -10,5 +10,25 @@ export default {
 </script>
 
 <style>
+
+.search {
+    border-radius: 7px 0px 7px 0px;
+    width: 250px;
+    padding: 6px;
+    background-color: rgb(255, 255, 255, 0.50);
+    border: none;
+    transition-delay: 0.4s;
+    box-shadow: 1.5px 2.5px rgb(0,0,0,0.25);
+}
+
+.search:focus{
+    box-shadow: 2.5px 3.5px rgb(0,0,0,0.25);
+    background-color: rgb(255, 255, 255, 0.75);
+    border-radius: 0px 7px 0px 7px;
+    outline: none
+}
+
+
+
 
 </style>
