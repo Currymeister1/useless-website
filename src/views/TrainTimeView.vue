7dda55a16934948e9b5d8cfb28323d43b86e6c4f
Tikshan Mohan <tik.mohan11@gmail.com>
2022-07-14 17:57:31 +0200

    Started with the train time

--- src/views/TrainTimeView.vue
+++ src/views/TrainTimeView.vue
@@ -1,12 +1,21 @@
 <template>
   <h1>Hello I am Train time</h1>
+
+  <NumberDisplay/>
+  <SearchBar/>
+
 </template>
 
 <script lang="ts">
-
+import SearchBar from '../components/SearchBar.vue';
+import NumberDisplay from '../components/NumberDisplay.vue';
 export default {
   
     name: 'TrainTimeView',
+    components: {
+      SearchBar,
+      NumberDisplay,
+    },
     methods: {
       loger(){
         console.log(1)
