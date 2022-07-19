77a3f4fae600a8babcae5453ac96866df0304cdc
Tikshan Mohan <tik.mohan11@gmail.com>
2022-07-19 17:55:44 +0200

    added the ability to filter

--- frontend/src/views/TrainTimeView.vue
+++ frontend/src/views/TrainTimeView.vue
@@ -1,8 +1,24 @@
 <template>
+  <div class="train">
   <h1>Hello I am Train time</h1>
 
   <NumberDisplay/>
-  <SearchBar @result="setQuery"/>
+    <div class="search">
+      <div class="station">
+       <SearchBar @result="setQuery"/>
+      </div>
+       <div class="line">
+       <SearchBar @result="setLine"/>
+      </div>
+       <div class="direction">
+       <SearchBar @result="setDirection"/>
+      </div>
+
+      <div class="btn">
+          <button @click="displayInfo">X</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -18,7 +34,10 @@ export default defineComponent({
           val: Number,
           divas: Divas,
           query: '' as string,
-          BASE_URL: 'http://localhost:3000/'
+          BASE_URL: 'http://localhost:3000/',
+          line: '' as string,
+          direction: '' as string, 
+          stopInfo: {} as any
         }
     },
     components: {
@@ -29,15 +48,37 @@ export default defineComponent({
       async setQuery(q:string){
         this.query = q;
         const diva = this.divas.find(d => d.PlatformText === q)?.DIVA;
-        const stopInfo = await this.getStopInfo(diva).then(console.log);
+        this.stopInfo = await this.getStopInfo(diva);
+        console.log(this.line)
 
-      },  
+       
+      },
+      
+      displayInfo(){
+        if(this.query != '' && this.line != '' && this.direction != ''){
+          for(let i = 0; i < this.stopInfo.data.monitors.length;i++){
+            if(this.stopInfo.data.monitors[i].lines[0].name == this.line){
+              console.log(this.stopInfo.data.monitors[i].lines[0].name)
+              if(this.stopInfo.data.monitors[i].lines[0].towards == this.direction){
+                console.log('Hello')
+              }
+            }
+          }
+        }
+       
+      },
 
       async getStopInfo(diva:any){
         console.log(`${this.BASE_URL}?diva=${diva}`)
         const response = await fetch(`${this.BASE_URL}?diva=${diva}`)
         return await response.json()
 
+      },
+      setLine(line:string){
+        this.line = line;
+      },
+      setDirection(direction:string){
+        this.direction = direction;
       }
 
     }
